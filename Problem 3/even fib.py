942ac8a50f18afd6829313aa57d72006f0e8a571
Accel-x <vegetasssuper@gmail.com>
2019-11-18 22:26:04 +0000

    First commit

---
+++ Problem 3/even fib.py
@@ -0,0 +1,13 @@
+n=int(input("Enter the number of fibonacci sequence: "))
+f_num=0
+s_num=1
+even_sum=0
+for i in range(0,n):
+    nx_num=f_num+s_num
+    f_num=s_num
+    s_num=nx_num
+    if nx_num%2==0:
+        even_sum+=nx_num
+        print(nx_num)
+print("The sum of even fibonacci sequence is :", even_sum)
+
